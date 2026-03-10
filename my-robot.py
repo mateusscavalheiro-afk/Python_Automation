8367f4976c38178e246cf7b39ad37e4f88b6ccb8
Mateus dos Santos Cavalheiro <mateus_s_cavalheiro@estudante.sesisenai.org.br>
2026-03-10 12:46:40 -0300

    Update my-robot.py

--- my-robot.py
+++ my-robot.py
@@ -1,1 +1,8 @@
-print("Hello World")
+import pyautogui as py
+import time as tm
+
+#First step: Open the Windows Menu
+py.press('Win')
+tm.sleep(3)
+py.write('bloco', interval = 0.1)
+py.press('Enter')
